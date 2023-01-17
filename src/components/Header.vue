00e89965cd52be52c56edb43782ff1a273eb7718
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-01-17 12:10:29 +0500

    added avatar in header and divider

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
-import Avatar from '@/components/Elements/Avatar.vue'
+import Avatar from "@/components/Elements/Avatar.vue";
 </script>
 
 <template>
-  <header class="bg-white absolute top-0 left-0 right-0 w-full shadow-sm flex items-center">
-    <div class="flex items-center justify-between">
-      <div>search</div>
-      <Avatar size="sm">
+  <header
+    class="bg-white absolute top-0 left-0 right-0 w-full shadow-sm flex items-center justify-between"
+  >
+    <div>search</div>
+    <div class="flex items-center">
+      <div class="w-px h-8 mx-4 bg-gray-200 "></div>
+      <Avatar class="mr-3" size="sm">
         <img src="@/assets/img/user.png" />
       </Avatar>
     </div>
--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -4,7 +4,7 @@ import Menu from "@/components/Menu/index.vue";
 </script>
 
 <template>
-  <Header class="h-16 ml-72" />
+  <Header class="h-16 pl-72" />
   <Menu class="w-72" />
   <main class="w-full bg-blue-50 pl-72 pt-16 min-h-screen">
     <slot></slot>
