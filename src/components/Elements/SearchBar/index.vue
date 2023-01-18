1ce03eb417af93f2c6d6513f20228e8396b9a641
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-01-18 12:05:35 +0500

    added search bar in the header

--- src/components/Elements/SearchBar/index.vue
+++ src/components/Elements/SearchBar/index.vue
@@ -1,40 +1,40 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import Button from '@/components/Elements/Button.vue'
-import { MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
+import { ref } from "vue";
+import Button from "@/components/Elements/Button.vue";
+import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
 import {
   Combobox,
   ComboboxInput,
   ComboboxOptions,
   ComboboxOption,
-} from '@headlessui/vue'
+} from "@headlessui/vue";
 
-const value = ref<any>(null)
+const value = ref<any>(null);
+const options = ["Durward Reynolds", "Kenton Towne"];
 
 const search = (event: Event): void => {
-  console.log('on change', event)
-}
+  console.log("on change", event);
+};
 </script>
 
 <template>
   <div>
-    <Button 
-      compact
-      pilled
-      class="p-2 rounded-full"
-    >
-      <MagnifyingGlassIcon class="h-6 w-6"></MagnifyingGlassIcon>
-    </Button>
     <Combobox v-model="value">
-      <ComboboxInput @change="search"></ComboboxInput>
-      <ComboboxOptions>
-        <ComboboxOption value="Durward Reynolds">
-          Durward Reynolds
-        </ComboboxOption>
-        <ComboboxOption value="Kenton Towne">
-          Kenton Towne
-        </ComboboxOption>
-      </ComboboxOptions>
+      <div class="relative">
+        <div class="flex bg-blue-50 py-2 px-4 rounded-full">
+          <ComboboxInput
+            class="outline-0 bg-transparent w-64 mr-2 px-1 text-md"
+            placeholder="Search"
+            @change="search"
+          ></ComboboxInput>
+          <MagnifyingGlassIcon class="h-7 w-7 text-gray-600"></MagnifyingGlassIcon>
+        </div>
+        <ComboboxOptions class="absolute min-w-full bg-white">
+          <ComboboxOption v-for="item in options" :value="item">
+            {{ item }}
+          </ComboboxOption>
+        </ComboboxOptions>
+      </div>
     </Combobox>
   </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@ import { BellIcon, ChatBubbleOvalLeftIcon } from '@heroicons/vue/24/solid'
 
 <template>
   <header class="bg-white absolute top-0 left-0 right-0 w-full shadow-sm flex items-center justify-between">
-    <SearchBar></SearchBar>
+    <SearchBar class="ml-4"></SearchBar>
     <div class="flex items-center">
       <Button compact variant="light" pilled class="p-2 mx-3 text-gray-700">
         <ChatBubbleOvalLeftIcon class="w-7" />
