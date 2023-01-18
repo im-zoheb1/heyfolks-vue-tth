ca2ce004f339011795f7996a93bf8c4b06bc7afe
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-01-18 12:58:41 +0500

    added autocomplete feature

--- src/components/Elements/SearchBar/index.vue
+++ src/components/Elements/SearchBar/index.vue
@@ -6,7 +6,7 @@ import {
   Combobox,
   ComboboxInput,
   ComboboxOptions,
-  ComboboxOption,
+  ComboboxOption
 } from "@headlessui/vue";
 
 const value = ref<any>(null);
@@ -25,15 +25,45 @@ const search = (event: Event): void => {
           <ComboboxInput
             class="outline-0 bg-transparent w-64 mr-2 px-1 text-md"
             placeholder="Search"
+            autocomplete="off"
             @change="search"
           ></ComboboxInput>
           <MagnifyingGlassIcon class="h-7 w-7 text-gray-600"></MagnifyingGlassIcon>
         </div>
-        <ComboboxOptions class="absolute min-w-full bg-white">
-          <ComboboxOption v-for="item in options" :value="item">
-            {{ item }}
-          </ComboboxOption>
-        </ComboboxOptions>
+        <transition
+          enter-active-class="transition duration-100 ease-out"
+          enter-from-class="transform scale-95 opacity-0"
+          enter-to-class="transform scale-100 opacity-100"
+          leave-active-class="transition duration-75 ease-in"
+          leave-from-class="transform scale-100 opacity-100"
+          leave-to-class="transform scale-95 opacity-0"
+        >
+          <ComboboxOptions
+            class="absolute mt-1 max-h-60 min-w-[380px] overflow-auto rounded-md bg-white py-2 
+              px-3 text-base shadow-lg ring-1 ring-slate-500 ring-opacity-5"
+          >
+            <ComboboxOption
+              v-for="item in options"
+              v-slot="{ selected, active }"
+              :value="item"
+            >
+              <li
+                class="relative select-none py-3 px-5 cursor-pointer rounded-md"
+                :class="{
+                  'bg-slate-100': active,
+                  'text-gray-900': !active,
+                }"
+              >
+                <span
+                  class="block truncate"
+                  :class="{ 'font-medium': selected, 'font-normal': !selected }"
+                >
+                  {{ item }}
+                </span>
+              </li>
+            </ComboboxOption>
+          </ComboboxOptions>
+        </transition>
       </div>
     </Combobox>
   </div>
