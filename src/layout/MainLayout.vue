399ad5dec9c1b1422dca359260e8f161c4193d12
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-01-18 22:30:31 +0500

    update: sidenav folder name from menu to sidebar

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import Header from "@/components/Header.vue";
-import Menu from "@/components/Menu/index.vue";
+import Sidebar from "@/components/Sidebar/index.vue";
 </script>
 
 <template>
   <Header class="h-16 pl-72" />
-  <Menu class="w-72" />
+  <Sidebar class="w-72" />
   <main class="w-full bg-blue-50 pl-72 pt-16 min-h-screen">
     <div class="p-3">
       <slot></slot>
