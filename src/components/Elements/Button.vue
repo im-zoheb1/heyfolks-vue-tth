e3c37f5a61ffdcae36565960498971e41b07b27e
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-01-27 03:16:04 +0500

    feat: added the feature in create post to add photos

--- src/components/Elements/Button.vue
+++ src/components/Elements/Button.vue
@@ -1,49 +1,51 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+
 const props = defineProps<{
   compact?: boolean;
   pilled?: boolean;
   type?: "submit" | "reset";
-  variant?: "primary" | "light";
+  variant?: "primary" | "light" | "light-primary" | "danger-light";
   size?: "sm" | "md" | "lg" | "xl";
   transformOnHover?: boolean;
   noPointerEvents?: boolean;
 }>();
 
-const variant: string = {
+const variant = ref<string>({
   primary: "bg-gradient-to-br from-primary to-secondary hover:from-primary/90 hover:to-secondary/90 text-white",
-  light: "bg-blue-50 hover:bg-blue-100 text-primary",
-}[props.variant ?? "primary"];
+  light: "bg-blue-50 hover:bg-blue-100 text-gray-600",
+  "light-primary": "bg-blue-50 hover:bg-blue-100 text-primary",
+  "danger-light": "bg-red-50 hover:bg-red-100 text-gray-500 shadow-xl"
+}[props.variant ?? "primary"]);
 
 // const spacing = ''
 
-const size = ((S: string): string => {
+const size = ref<string>(((S: string): string => {
   let [padding, text]: [string, string] = ['', '']
   if (S === 'xl') [padding, text] = ['py-3.5 px-9', 'text-xl']
   if (S === 'lg') [padding] = ['py-2.5 px-5', 'text-lg']
   if (S === 'md') [padding] = ['py-2 px-3', 'text-base']
-  if (S === 'sm') [padding] = ['p-0', 'text-md']
+  if (S === 'sm') [padding] = ['py-1.5 px-3', 'text-md']
   return props.compact ? text : padding + ' ' + text
-})(props.size || 'md')
+})(props.size || 'md'))
 
 /* const spacing: string = {
   compact: "",
   normal: "py-3.5 px-9 text-xl",
 }[props.compact ? "compact" : "normal"]; */
 
-const pointerEvents = props.noPointerEvents ? 'pointer-events-none' : ''
+const pointerEvents = ref<string>(props.noPointerEvents ? 'pointer-events-none' : '')
 
-const border: string = {
+const border = ref<string>({
   pilled: "rounded-full",
   rounded: "rounded-lg",
-}[props.pilled ? "pilled" : "rounded"];
+}[props.pilled ? "pilled" : "rounded"]);
 
-const transform: string = props.transformOnHover
-  ? "hover:translate-y-[-4px] active:translate-y-[4px]"
-  : "";
+const transform = ref<string>(props.transformOnHover ? "hover:translate-y-[-4px] active:translate-y-[4px]" : "");
 
 const defaultStyles = "font-bold transition duration-300 ease-in-out";
 
-const style: string = `${variant} ${size} ${border} ${defaultStyles} ${transform} ${pointerEvents}`;
+const style = ref<string>(`${variant.value} ${size.value} ${border.value} ${defaultStyles} ${transform.value} ${pointerEvents.value}`);
 </script>
 
 <template>
--- src/components/Elements/CreatePost/index.vue
+++ src/components/Elements/CreatePost/index.vue
@@ -3,25 +3,25 @@ import Separator from '@/components/Elements/Separator.vue'
 import Button from '@/components/Elements/Button.vue'
 import InputFile from '@/components/Elements/Form/InputFile.vue'
 import { faker } from '@faker-js/faker'
-import { PhotoIcon } from '@heroicons/vue/24/solid';
+import { PhotoIcon, TrashIcon } from '@heroicons/vue/24/solid';
 import { ref } from 'vue';
 
 const firstName: string = faker.name.firstName()
 const placeholder: string = `What's been on your mind lately, ${firstName}?`
 
-const uploadPhotos = ref<any>([])
+const photos = ref<any>([])
 
 const onLoad = (e: Event): void => {
-  for (let file of uploadPhotos.value) {
+  for (let file of photos.value) {
     URL.revokeObjectURL(file)
   }
 }
 
 const onChange = (e: Event): void => { 
-  uploadPhotos.value = []
+  photos.value = []
   const files: FileList | null  = (e.target as HTMLInputElement).files
   if (files) for (let file of files) {
-    uploadPhotos.value.push(URL.createObjectURL(file))
+    photos.value.push(URL.createObjectURL(file))
   }
 }
 </script>
@@ -29,14 +29,33 @@ const onChange = (e: Event): void => {
 <template>
   <div class="px-4 py-2">
     <textarea class="w-full bg-white outline-0 text-gray-700 text-lg resize-none tracking-wide" :placeholder="placeholder" rows="3"></textarea>
+    <div class="flex mb-3">
+      <!-- start: selected photos -->
+      <div 
+        class="create-post-photo relative w-40 h-40 border cursor-pointer rounded-lg
+          before:absolute before:inset-0 before:hover:backdrop-blur-sm before:hover:bg-black/20 
+          before:rounded-lg before:transition before:duration-300" 
+        v-for="photo in photos"
+      >
+        <img class="w-full h-full object-cover rounded-lg" :src="photo" />
+        <Button variant="light" class="absolute top-1 right-1 p-1 shadow-sm" size="sm" compact pilled>
+          <TrashIcon class="w-5 h-5 cursor-pointer font-bold" />
+        </Button>
+      </div>
+      <!-- end: selected-photos -->
+    </div>
     <Separator />
-    <div class="flex pt-2">
+    <div class="flex items-center justify-between pt-2">
+      <!-- start: footer  -->
       <InputFile name="upload-photo-file" multiple @change="onChange" @load="onLoad">
         <Button variant="light" class="flex items-center justify-center text-green-600 w-11 h-11 bg-green-100" pilled no-pointer-events>
           <PhotoIcon class="w-6 h-6" />
         </Button>
       </InputFile>
-      <Button class="px-[20px] ml-auto" size="sm">Post</Button>
+      <Button class="px-[22px]" size="md">Create</Button>
+      <!-- end: footer -->
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped></style>
