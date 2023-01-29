d876425e26d22394cae48ac1da54e7fe89b50eaf
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-01-29 16:49:39 +0500

    feat: added post card header

--- src/components/Elements/Cards/PostCard/index.vue
+++ src/components/Elements/Cards/PostCard/index.vue
@@ -1,7 +1,22 @@
 <script lang="ts" setup>
 import Card from '@/components/Elements/Cards/index.vue'
+import Avatar from '@/components/Elements/Avatar.vue'
+
+const props = defineProps<{
+  value: any
+}>()
 </script>
 
 <template>
-  <Card>post card</Card>
+  <Card class="p-3">
+    <div class="flex items-center">
+      <Avatar size="sm">
+        <img :src="value.avatar" />
+      </Avatar>
+      <span class="leading-tight ml-3">
+        <div class="text-md">{{ value.fullname }}</div>
+        <div class="text-gray-400 text-sm">{{ value.city }}</div>
+      </span>
+    </div>
+  </Card>
 </template>
--- src/modules/dashboard/Home/index.vue
+++ src/modules/dashboard/Home/index.vue
@@ -2,13 +2,34 @@
 import MainLayout from "@/layout/MainLayout.vue";
 import CreatePost from "@/components/Elements/CreatePost/index.vue";
 import PostCard from "@/components/Elements/Cards/PostCard/index.vue"
+import { faker } from "@faker-js/faker";
+import { ref, onMounted } from "vue";
+
+const getFakePost = (): any => {
+  const fullname: string =  faker.name.fullName();
+  const avatar: string = faker.image.avatar();
+  const city: string = faker.address.city()
+  return {
+    avatar,
+    fullname,
+    city,
+  }
+}
+
+const posts = ref<any>([]);
+
+onMounted(() => {
+  for (let i = 0; i < 25; i++) {
+    posts.value.push(getFakePost())
+  }
+})
 </script>
 
 <template>
   <MainLayout>
-    <div class="gap-3 columns-2 [&>*]:mb-3">
+    <div class="gap-3 columns-[30rem] [&>*]:mb-3">
       <CreatePost />
-      <PostCard v-for="i in 100"></PostCard>
+      <PostCard v-for="post in posts" :value="post" />
     </div>
   </MainLayout>
 </template>
