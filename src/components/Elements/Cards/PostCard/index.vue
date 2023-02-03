c53af19445728bb92e31fe8bccb00b7496b2a852
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-04 03:35:49 +0500

    post card actions sections updated

--- src/components/Elements/Cards/PostCard/index.vue
+++ src/components/Elements/Cards/PostCard/index.vue
@@ -70,17 +70,17 @@ const toggleSave = (): void => {
 
     <!-- start: interactions start -->
     <Separator />
-    <div class="flex py-2 px-3 text-sm font-semibold">
-      <div class="flex items-center mr-6 cursor-pointer w-24" :class="{ 'font-bold text-primary': isLiked }" @click="toggleLike">
+    <div class="flex text-sm font-semibold">
+      <div class="flex items-center justify-center cursor-pointer w-4/12 hover:bg-blue-50 py-2" :class="{ 'font-bold text-primary': isLiked }" @click="toggleLike">
         <LikeSolidIcon v-if="isLiked" class="w-6" />
         <LikeIcon v-else class="w-6" />
         <div class="ml-2">{{ likesCount }} Likes</div>
       </div>
-      <div class="flex items-center mr-6 cursor-pointer w-32">
+      <div class="flex items-center justify-center cursor-pointer w-4/12 hover:bg-blue-50 py-2">
         <CommentIcon class="w-6" />
         <div class="ml-2">{{ commentsCount }} comments</div>
       </div>
-      <div class="flex items-center cursor-pointer w-28" @click="toggleSave">
+      <div class="flex items-center justify-center cursor-pointer w-4/12 hover:bg-blue-50 py-2" @click="toggleSave">
         <SaveSolidIcon v-if="isSaved" class="w-6"/>
         <SaveIcon v-else class="w-6"/>
         <div class="ml-2">Save</div>
@@ -89,3 +89,6 @@ const toggleSave = (): void => {
     <!-- end: interactions end -->
   </Card>
 </template>
+
+<style lang="scss" scoped>
+</style>
