abb1b59ef13c974dca541572a6c58a0791f1f776
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-05 16:34:21 +0500

    added comment input

--- src/components/Elements/Comments/index.vue
+++ src/components/Elements/Comments/index.vue
@@ -1,7 +1,36 @@
 <script lang="ts" setup>
+import Separator from '@/components/Elements/Separator.vue';
+import Button from '@/components/Elements/Button.vue'
+import Avatar from '@/components/Elements/Avatar.vue';
+import { PaperAirplaneIcon } from '@heroicons/vue/24/outline';
+import { faker } from '@faker-js/faker';
+import { ref } from 'vue';
+
+const avatar: string = faker.internet.avatar()
+
+const isCommentActive = ref<boolean>(false)
 </script>
 
 <template>
-  <div class="shadow-inner bg-light-1 p-2 h-full">
+  <div class="bg-light-1 h-full shadow-inner">
+    <div class="flex items-center p-2">
+      <Avatar size="xs">
+        <img :src="avatar" />
+      </Avatar>
+      <a 
+        class="flex items-center rounded-lg w-full ml-2 bg-white p-1 border"
+        :class="{ 'ring-2': isCommentActive }"
+      >
+        <input 
+          class="w-full outline-none text-base rounded-lg px-2" 
+          placeholder="Share your thoughts" 
+          @focus="isCommentActive = true" 
+          @blur="isCommentActive = false" 
+        />
+        <Button compact class="p-1.5">
+          <PaperAirplaneIcon class="h-6" />
+        </Button>
+      </a>
+    </div>
   </div>
 </template>
