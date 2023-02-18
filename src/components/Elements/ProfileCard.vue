bb53864177c944dcadd3e528eed8470b0b3bab63
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-18 14:28:02 +0500

    changed profile card design

--- src/components/Elements/ProfileCard.vue
+++ src/components/Elements/ProfileCard.vue
@@ -1,44 +1,52 @@
 <script lang="ts" setup>
-import Card from './Card.vue';
-import Avatar from './Avatar.vue';
-import Separator from './Separator.vue';
-import Menu from './Menu';
-import MenuItem from './Menu/MenuItem.vue';
-import { 
+import Card from "./Card.vue";
+import Avatar from "./Avatar.vue";
+import Separator from "./Separator.vue";
+import Button from "./Button.vue";
+import Menu from "./Menu";
+import MenuItem from "./Menu/MenuItem.vue";
+import {
   EllipsisHorizontalIcon,
   ArchiveBoxXMarkIcon as UnfollowIcon,
-  ChatBubbleLeftEllipsisIcon as MessageIcon
-} from '@heroicons/vue/24/outline';
+  ChatBubbleLeftEllipsisIcon as MessageIcon,
+} from "@heroicons/vue/24/outline";
 
 const props = defineProps<{
-  data: any
-}>()
+  data: any;
+}>();
 </script>
 
 <template>
   <Card class="px-3 py-4 shadow-sm cursor-pointer">
-    <div class="flex items-center leading-none">
+    <div class="flex items-center leading-tight">
       <Avatar size="md">
         <img :src="data.avatar" />
       </Avatar>
-      <div class="mx-5">
-        <h3 class="font-semibold mb-1">{{ data.fullname }}</h3>
-        <h3 class="text-muted line-clamp-1">{{ data.city }}</h3>
+      <div class="w-full ml-5">
+        <div class="flex items-center justify-between w-full mb-3">
+          <div>
+            <h3 class="font-semibold mb-1">{{ data.fullname }}</h3>
+            <h3 class="text-muted line-clamp-1">{{ data.city }}</h3>
+          </div>
+          <Menu class="ml-auto">
+            <template #button>
+              <Button variant="light" class="p-1" compact pilled>
+                <EllipsisHorizontalIcon class="w-6" />
+              </Button>
+            </template>
+            <MenuItem class="flex items-center font-bold text-sm">
+              <UnfollowIcon class="w-5 mr-2" />
+              Unfollow
+            </MenuItem>
+            <Separator />
+            <MenuItem class="flex items-center font-bold text-sm">
+              <MessageIcon class="w-5 mr-2" />
+              Message
+            </MenuItem>
+          </Menu>
+        </div>
+        <div>{{ data.bio }}</div>
       </div>
-      <Menu class="ml-auto">
-        <template #button>
-          <EllipsisHorizontalIcon class="w-6" />
-        </template>
-        <MenuItem class="flex items-center font-bold text-sm">
-          <UnfollowIcon class="w-5 mr-2" />
-          Unfollow
-        </MenuItem>
-        <Separator />
-        <MenuItem class="flex items-center font-bold text-sm">
-          <MessageIcon class="w-5 mr-2" />
-          Message
-        </MenuItem>
-      </Menu>
     </div>
   </Card>
 </template>
