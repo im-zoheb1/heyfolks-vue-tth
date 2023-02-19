affebc5879b10d423392e6332d81a96ec4c5f618
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-19 14:18:30 +0500

    added chat footer

--- src/modules/dashboard/Messages/index.vue
+++ src/modules/dashboard/Messages/index.vue
@@ -2,27 +2,50 @@
 import MainLayout from "@/layout/MainLayout.vue";
 import Separator from "@/components/Elements/Separator.vue";
 import ChatList from "@/components/Elements/ChatList.vue";
+import CommentInput from "@/components/Elements/Comments/CommentInput.vue";
+import { ref } from "vue";
+
+const message = ref<string>('')
 </script>
 
 <template>
   <MainLayout>
-    <div class="chat">
+    <div class="chat-container">
       <ChatList />
       <Separator is-vertical />
-      <div class="chat__user-chat">right</div>
+      <div class="chat">
+        <div class="chat__header">chat header</div>
+        <div class="chat__content">
+          <div class="chat__messages">
+            <div class="chat__message">messaages</div>
+          </div>
+        </div>
+        <Separator />
+        <div class="chat__footer">
+          <CommentInput v-model="message" />
+        </div>
+      </div>
     </div>
   </MainLayout>
 </template>
 
 <style lang="scss" scoped>
-.chat {
+.chat-container {
   height: calc(100vh - 88px);
   @apply  bg-main-bg shadow-sm flex items-stretch;
-  &__left-sidebar {
-    @apply flex-1 p-2;
+}
+.chat {
+  @apply flex flex-col flex-[2];
+  &__header, &__content, &__footer {
+    @apply p-2;
   }
-  &__user-chat {
-    @apply flex-[2] p-2;
+  &__header {
+    @apply shadow-sm;
+  }
+  &__content {
+    @apply flex-1 overflow-y-scroll;
+  }
+  &__footer {
   }
 }
 </style>
