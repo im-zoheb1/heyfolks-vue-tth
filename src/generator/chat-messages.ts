54bfb68a5c4912ce5ba7b98746f1a43314244a21
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-19 23:14:11 +0500

    added fake data for the chat

--- src/generator/chat-messages.ts
+++ src/generator/chat-messages.ts
@@ -1,5 +1,28 @@
-export const getPersonInfo = () => { }
+import { faker } from "@faker-js/faker"
 
-export const getMessages = () => { }
-
-export const getChat = () => { }
+export const getChat = () => {
+  const messages = []
+  let sender = {
+    id: 1,
+    name: faker.name.fullName(),
+    avatar: faker.internet.avatar()
+  }
+  let recipient = {
+    id: 2,
+    name: faker.name.fullName(),
+    avatar: faker.internet.avatar()
+  }
+  const userInfo = recipient
+  for (let i = 0; i < 20; i++) {
+    if (faker.datatype.boolean()) {
+      [recipient, sender] = [sender, recipient]
+    }
+    messages.push({ 
+      sender, 
+      recipient,
+      content: faker.lorem.sentence(),
+      timestamp: faker.date.recent(),
+    })
+  }
+  return { userInfo, messages }
+}
