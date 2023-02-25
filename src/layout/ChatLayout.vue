4ef6227e5a47da24774663bebc1d25449ec1fffb
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-25 11:59:07 +0500

    fixed the chat layout for mobile version

--- src/layout/ChatLayout.vue
+++ src/layout/ChatLayout.vue
@@ -11,7 +11,7 @@ const userData: any = reactive<any>(getUserData());
   <Header class="h-16 z-header-tab" :user-data="userData" />
   <Sidenav is-collapsed class="z-sidenav sm:w-20 sm:block border-r" :user-data="userData" />
   <main
-    class="w-full bg-light-1 pt-16 min-h-screen pb-16 sm:pl-20 sm:pb-0"
+    class="w-full bg-light-1 pt-16 min-h-screen sm:pl-20 sm:pb-0"
   >
     <div>
       <slot></slot>
