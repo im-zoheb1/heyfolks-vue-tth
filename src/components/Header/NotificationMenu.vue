1c717d794d488c0948f36299b5d9ab439c9c8e1f
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-25 19:30:23 +0500

    made notification list a common component

--- src/components/Header/NotificationMenu.vue
+++ src/components/Header/NotificationMenu.vue
@@ -2,12 +2,9 @@
 import Popover from "@/components/Elements/Popover.vue";
 import Button from "@/components/Elements/Button.vue";
 import Separator from "@/components/Elements/Separator.vue";
-import Avatar from "@/components/Elements/Avatar.vue";
-import { BellIcon, CheckIcon } from "@heroicons/vue/20/solid";
-import { ref } from "vue";
-import { getNotifications } from "@/generator/notifications";
+import { BellIcon } from "@heroicons/vue/20/solid";
+import NotificationList from "../Elements/NotificationList.vue";
 
-const notifications = ref<any[]>(getNotifications());
 </script>
 
 <template>
@@ -19,50 +16,7 @@ const notifications = ref<any[]>(getNotifications());
     </template>
     <template #content>
       <div class="flex flex-col h-full">
-        <div>
-          <div class="flex items-center justify-between py-3 px-4">
-            <h3 class="text-xl tracking-wide font-bold">Notifications</h3>
-            <a href="#" class="flex text-md font-semibold text-primary hover:underline">
-              <CheckIcon class="w-5" />
-              <CheckIcon class="w-5 -translate-x-3" />
-              <span class="-translate-x-2">Mark all as read</span>
-            </a>
-          </div>
-          <Separator />
-        </div>
-
-        <div class="p-2 flex-1">
-          <a
-            href="#"
-            v-for="(notification, index) in notifications"
-            class="flex p-3 pr-8 hover:bg-light-2 hover:border-transparent transition duration-300 relative"
-            :class="{
-              'bg-light-1': notification.isNew,
-              'border-t': index,
-              'after:content-[\'\'] after:w-2.5 after:h-2.5 after:bg-primary after:absolute after:right-2 after:top-3 after:rounded-full':
-                notification.isNew,
-            }"
-          >
-            <Avatar size="sm" :src="notification.avatar" />
-            <div class="pl-4">
-              <div>
-                <span class="font-bold">{{ notification.name }}</span>
-                {{ notification.comment }}
-              </div>
-              <div class="text-primary font-semibold text-sm mt-1">
-                {{ $moment(notification.date).fromNow() }}
-              </div>
-              <div
-                v-if="notification.type === 'follow-request'"
-                class="flex items-center mt-1"
-              >
-                <Button class="mr-3" size="sm">Accept</Button>
-                <Button variant="light" size="sm">Decline</Button>
-              </div>
-            </div>
-          </a>
-        </div>
-
+        <NotificationList />
         <div>
           <Separator />
           <Button class="px-4 m-3" size="sm" variant="light">
--- src/modules/dashboard/Notifications/index.vue
+++ src/modules/dashboard/Notifications/index.vue
@@ -1,14 +1,16 @@
 <script lang="ts" setup>
+import NotificationList from "@/components/Elements/NotificationList.vue";
 import MainLayout from "@/layout/MainLayout.vue";
 </script>
 
 <template>
   <MainLayout no-padding>
     <div class="flex gap-3 items-start">
-      <Card class="bg-main-bg shadow-sm p-3 rounded flex-[2] min-h-[calc(100vh-85px)]">
-        <h3 class="font-bold text-xl">Notifications</h3>
+      <Card class="bg-main-bg shadow-sm rounded flex-[2] min-h-[calc(100vh-85px)]">
+        <!-- <h3 class="font-bold text-xl">Notifications</h3> -->
+        <NotificationList />
       </Card>
-      <Card class="flex-1 bg-main-bg shadow-sm p-3">
+      <Card class="flex-1 bg-main-bg shadow-sm p-3 max-lg:hidden">
         <h3 class="font-bold text-xl">Suggesstions</h3>
       </Card>
     </div>
