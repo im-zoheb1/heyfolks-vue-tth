9ef7b9b7fb1112c7490aede3a83c3a29e1bad4e8
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-02-26 14:04:11 +0500

    adding profile section

--- src/modules/dashboard/Profile/index.vue
+++ src/modules/dashboard/Profile/index.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup>
 import MainLayout from '@/layout/MainLayout.vue'
+import SuggesstionsCard from '@/components/Elements/SuggesstionsCard.vue';
+import Card from '@/components/Elements/Card.vue';
 </script>
 
 <template>
-  <MainLayout>Profile</MainLayout>
+  <MainLayout>
+    <div class="flex items-start gap-3">
+      <Card class="flex-[2]">profile</Card>
+      <SuggesstionsCard class="flex-1" />
+    </div>
+  </MainLayout>
 </template>
