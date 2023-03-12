b76fbc09ec8ce26bf59fa136a66f13e23d3f1763
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-03-12 20:40:10 +0500

    set types to settimeout in profile tooltip

--- src/components/Elements/ProfileTooltip.vue
+++ src/components/Elements/ProfileTooltip.vue
@@ -14,7 +14,7 @@ interface PositionStyleInterface {
 	bottom?: string;  
 }
 
-const tooltipTimeout = ref<null | number>(null)
+const tooltipTimeout = ref<number>()
 const tooltipVisible = ref<boolean>(false)
 const position = ref<PositionStyleInterface>({ top: '', left: '', bottom: '' })
 const data = ref((() => {
