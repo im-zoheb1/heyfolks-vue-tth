1a4f954a0ba11c54ad74e01237c345d7b9957474
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-03-12 13:00:47 +0500

    make the popover visible on dialog overflow auto

--- src/components/Elements/ProfileTooltip.vue
+++ src/components/Elements/ProfileTooltip.vue
@@ -8,9 +8,15 @@ import {
   MapPinIcon as LocationIcon 
 } from '@heroicons/vue/20/solid';
 
+interface PositionStyleInterface { 
+	top?: string; 
+	left?: string; 
+	bottom?: string;  
+}
+
 const tooltipTimeout = ref<null | number>(null)
 const tooltipVisible = ref<boolean>(false)
-const position = ref<{ top: string; left: string  }>({ top: '', left: '' })
+const position = ref<PositionStyleInterface>({ top: '', left: '', bottom: '' })
 const data = ref((() => {
   const avatar = faker.internet.avatar()
   const fullname = faker.name.fullName()
@@ -21,10 +27,32 @@ const data = ref((() => {
   return { avatar, fullname, username, contacts, bio, city }
 })())
 
-const displayTooltip = (e: MouseEvent): void => {
+type Position = 'top' | 'bottom'
+const setPosition = (P: Position, E: MouseEvent): void => {
+	const el = (E.target as HTMLDivElement)
+  const { top, left, bottom } = el.getBoundingClientRect()
+	if (P === 'top') { 
+    position.value = { 
+      bottom: bottom + el.offsetHeight + 'px', 
+      left: left + 'px', 
+      top: 'auto'
+    }
+	} else {
+		position.value = { 
+      top: top + el.offsetHeight + 'px', 
+      left: left + 'px',
+    }
+	}
+}
+
+const displayTooltip = (E: MouseEvent): void => {
   const wHeight = window.innerHeight // window height
   const wHalf = wHeight / 2 // window half
-  position.value = { top: e.clientY + 'px', left: e.clientX + 'px' }
+	if (wHalf > (E.target as HTMLDivElement).offsetTop) {
+		setPosition('bottom', E)
+	} else {
+		setPosition('top', E)
+	}
   tooltipTimeout.value = setTimeout(() => (tooltipVisible.value = true), 500) 
 }
 
@@ -40,34 +68,34 @@ const hideTooltip = (e: MouseEvent): void => {
       <slot>Profile Tooltip</slot>
     </span>
     <Teleport to="body">
-      <div 
-        class="fixed bg-main-bg shadow-xl rounded-xl border w-80 p-3 z-tooltip top-0 right-0 -translate-x-[50%] hover:visible"
-        :class="{ 'hidden': !tooltipVisible }"
-        :style="position"
-        @mouseenter="tooltipVisible = true" 
-        @mouseleave="hideTooltip"
-      > 
-        <div class="flex items-center gap-3 mb-2">
-          <Avatar :src="data.avatar" size="lg" />
-          <div>
-            <h3>{{ data.fullname }}</h3>
-            <h3 class="text-muted text-sm">@{{ data.username }}</h3>
-            <div class="mt-1.5 text-sm">{{ data.contacts }} <span class="text-muted">Connections</span></div>
-          </div> 
-        </div>
-        <div>
-          <p class="mt-3 text-muted flex font-normal text-md">
-            <LocationIcon class="w-5 mr-1 mb-1" /> {{ data.city }}
-          </p>
-          <p class="mt-1 font-normal">{{ data.bio }}</p>
-        </div>
-        <div class="flex ml-auto mt-3">
-          <Button class="flex-1" variant="primary" pilled>Follow</Button>
-          <Button pilled compact class="ml-2" variant="light" @click="() => $router.push({ name: 'messages' })">
-            <MessageIcon class="w-5 m-2" />
-          </Button>
-        </div>
-      </div>
+			<div 
+				v-if="tooltipVisible"
+				class="fixed bg-main-bg shadow-xl rounded-xl border w-96 p-3 z-tooltip top-0 right-0"
+				:style="position"
+				@mouseenter="tooltipVisible = true" 
+				@mouseleave="hideTooltip"
+			> 
+				<div class="flex items-center gap-3 mb-2">
+					<Avatar :src="data.avatar" size="lg" />
+					<div>
+						<h3>{{ data.fullname }}</h3>
+						<h3 class="text-muted text-sm">@{{ data.username }}</h3>
+						<div class="mt-1.5 text-sm">{{ data.contacts }} <span class="text-muted">Connections</span></div>
+					</div> 
+				</div>
+				<div>
+					<p class="mt-3 text-muted flex font-normal text-md">
+						<LocationIcon class="w-5 mr-1 mb-1" /> {{ data.city }}
+					</p>
+					<p class="mt-1 font-normal">{{ data.bio }}</p>
+				</div>
+				<div class="flex ml-auto mt-3">
+					<Button class="flex-1" variant="primary" pilled>Follow</Button>
+					<Button pilled compact class="ml-2" variant="light" @click="() => $router.push({ name: 'messages' })">
+						<MessageIcon class="w-5 m-2" />
+					</Button>
+				</div>
+			</div>
     </Teleport>
   </a>
 </template>
