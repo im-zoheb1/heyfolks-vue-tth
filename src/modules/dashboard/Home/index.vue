5ce9ab24e6808a86d3cb963fbb0ac887c4dfe68b
zohaibkhan-wiot <zohaib.khan@wiot360.com>
2023-03-21 00:22:27 +0500

    added circular loader at the end in home page for loading more posts

--- src/modules/dashboard/Home/index.vue
+++ src/modules/dashboard/Home/index.vue
@@ -2,6 +2,7 @@
 import MainLayout from "@/layout/MainLayout.vue";
 import CreatePost from "@/components/Elements/CreatePost/index.vue";
 import PostCard from "@/components/Elements/PostCard/index.vue"
+import SeekingLoader from '@/components/Elements/Loaders/Seeking.vue'
 import { ref } from "vue";
 import { getPosts } from "@/generator/posts";
 
@@ -23,6 +24,9 @@ const posts = ref<any[]>(getPosts());
 				:key="`home-post-card-${index}`"
 				:value="post" 
 			/>
+    </div>
+    <div class="h-24 grid place-content-center">
+      <SeekingLoader />
     </div>
   </MainLayout>
 </template>
